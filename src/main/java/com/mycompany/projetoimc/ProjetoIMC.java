54813fa59662f9a5b7b0f20859262f7f5672dd82
Maria1Edu <989963154lei@gmail.com>
2023-05-22 21:41:05 -0300

    Programa exemplo da aula de segunda feira 22/05 - primeira versão

---
+++ src/main/java/com/mycompany/projetoimc/ProjetoIMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoimc;
+
+/**
+ *
+ * @author Roberlei
+ */
+public class ProjetoIMC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
